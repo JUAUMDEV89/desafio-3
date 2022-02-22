58915a24ca6d8de2ca4893768bf133ec6bbb98c0
JUAUMDEV89 <jlrmd89@gmail.com>
2022-02-22 15:15:26 -0300

    page home loadProducts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,24 +23,32 @@ interface CartItemsAmount {
 
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+   const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    const newSumAmount = {...sumAmount};
+    newSumAmount[product.id] = product.amount;
+
+    return newSumAmount;
+   }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      const response = await api.get('/products');
+      const response = await api.get<Product []>('/products');
 
-      setProducts(response.data)
+      const data = response.data.map(product=>({
+        ...product,
+        priceFormated: formatPrice(product.price)
+      }));
+
+      setProducts(data)
     }
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id)
   }
 
   return (
@@ -59,7 +67,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
+               {cartItemsAmount[product.id] || 0} 
             </div>
   
             <span>ADICIONAR AO CARRINHO</span>
